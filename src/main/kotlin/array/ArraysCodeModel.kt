8b6ddbfe6200061e160e08a58c85e1b7dd2c4822
fritzXu <572789993@qq.com>
2024-03-16 16:54:57 +0800

    leetCode 1089. 复写零(middle)

--- src/main/kotlin/array/ArraysCodeModel.kt
+++ src/main/kotlin/array/ArraysCodeModel.kt
@@ -1469,6 +1469,51 @@ class ArraysCodeModel {
         }
         return ans.sum() + count
     }
+
+    /**
+     * leetCode 1089. 复写零(middle)
+     * 给你一个长度固定的整数数组 arr ，请你将该数组中出现的每个零都复写一遍，并将其余的元素向右平移。
+     * 注意：请不要在超过该数组长度的位置写入元素。请对输入的数组 就地 进行上述修改，不要从函数返回任何东西。
+     *
+     * 示例 1：
+     * 输入：arr = [1,0,2,3,0,4,5,0]
+     * 输出：[1,0,0,2,3,0,0,4]
+     * 解释：调用函数后，输入的数组将被修改为：[1,0,0,2,3,0,0,4]
+     * 示例 2：
+     * 输入：arr = [1,2,3]
+     * 输出：[1,2,3]
+     * 解释：调用函数后，输入的数组将被修改为：[1,2,3]
+     *
+     * 提示:双指针的灵活使用
+     */
+    fun duplicateZeros(arr: IntArray) {
+        var index = 0
+        var position = 0
+        while (position < arr.size) {
+            if (arr[index] == 0) {
+                position++
+            }
+            index++
+            position++
+        }
+        index--
+        position--
+        while (index >= 0) {
+            if (position > 0 && position < arr.size) {
+                arr[position] = arr[index]
+            }
+            if (position > 0 && arr[index] == 0) {
+                position--
+                arr[position] = 0
+            }
+            index--
+            position--
+        }
+        arr.forEach {
+            print(",$it")
+        }
+        println()
+    }
 }
 
 fun main() {
@@ -1489,9 +1534,7 @@ fun main() {
 //    println(item.totalFruit(intArrayOf(3, 3, 3, 1, 2, 1, 1, 2, 3, 3, 4))) // 5
 //    println(item.totalFruit(intArrayOf(0, 0, 1, 1))) // 4
 //    println(item.totalFruit(intArrayOf(0, 1, 6, 6, 4, 4, 6))) // 5
-    println(item.maxSatisfied(intArrayOf(1, 0, 1, 2, 1, 1, 7, 5), intArrayOf(0, 1, 0, 1, 0, 1, 0, 1), 3)) // 16
-    println(item.maxSatisfied(intArrayOf(5, 8), intArrayOf(0, 1), 1)) // 13
-    println(item.maxSatisfied(intArrayOf(6, 10, 2, 1, 7, 9), intArrayOf(1, 0, 0, 0, 0, 1), 3)) // 29
+    println(item.duplicateZeros(intArrayOf(1, 0, 2, 3, 0, 4, 5, 0)))
 
 
 }
